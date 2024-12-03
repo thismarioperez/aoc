7a9e8f503e5f298f1bab266d6359cd6c6a6aba65
Mario Perez <mario.perez415@gmail.com>
2024-12-03 15:50:26 -0500

    Update day 3 part a solution

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,25 +1,25 @@
 import { runSolution } from "../utils.ts";
 
-const COMMAND_EXPRESSION = /mul\(\d{1,3},\d{1,3}\)/g;
+const MUL_EXPRESSION = /mul\(\d{1,3},\d{1,3}\)/g;
 const INPUTS_EXPRESSION = /\d{1,3}/g;
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
     // console.log(data);
 
-    const commands = data.join("").match(COMMAND_EXPRESSION);
+    const commands = data.join("").match(MUL_EXPRESSION);
 
     // console.log(commands);
 
     let inputs = [];
-    const total = commands.reduce((acc, command) => {
+    let total = 0;
+
+    for (const command of commands) {
         inputs = command.match(INPUTS_EXPRESSION);
         inputs = inputs.map((str) => parseInt(str));
 
-        // console.log(inputs);
-
-        return (acc += inputs[0] * inputs[1]);
-    }, 0);
+        total += inputs[0] * inputs[1];
+    }
 
     return total;
 }
