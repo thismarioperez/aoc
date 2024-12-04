5b98863d0d5d9c369f53d94a09803a8e8356253d
Mario Perez <mario.perez415@gmail.com>
2024-12-04 02:36:13 -0500

    Solve day 4 part a

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,21 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
+import {
+    searchMatrixForStringOccurences,
+    type Matrix,
+} from "../helpers/matrix.ts";
 
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+    console.log(data);
+
+    // convert to matrix
+    const matrix: Matrix<string> = data.map((x) => x.split(""));
+
+    let count = 0;
+
+    count += searchMatrixForStringOccurences(matrix, "XMAS");
+
+    return count;
 }
 
 await runSolution(day4a);
