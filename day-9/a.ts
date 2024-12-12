a7d2b332c3025c78069e39788c85e7d80319f127
Mario Perez <mario.perez415@gmail.com>
2024-12-11 20:36:51 -0500

    Improve part a answer

--- day-9/a.ts
+++ day-9/a.ts
@@ -1,70 +1,70 @@
 import { runSolution } from "../utils.ts";
 
-type FreeSpace = ".";
-type FileId = number;
-type Block = string;
+type Byte = number | null;
+type Disk = Byte[];
 
-const FREE_SPACE: FreeSpace = ".";
-
-const parseData = (data: string[]): number[] => {
-    return data[0].split("").map((x) => parseInt(x));
-};
-
-const isEven = (x: number) => x % 2 == 0;
-
-const unpackData = (data: number[]): Block[] => {
-    let id: FileId | null = null;
-    return data.reduce((acc, num, idx) => {
-        const space = new Array(num);
-        if (idx === 0 || isEven(idx)) {
-            id = idx / 2;
-            space.fill(id.toString());
-        } else {
-            space.fill(FREE_SPACE);
+const parseData = (data: string[]): Disk => {
+    return data[0].split("").reduce((acc, char, i) => {
+        // push values as many times as the given char represents
+        for (let j = 0; j < Number(char); ++j) {
+            if (i % 2 === 0) {
+                // we're in a file block
+                // the id is the current index divided by 2, since spaces exist in between file blocks
+                acc.push(i / 2);
+            } else {
+                // we're in a space block
+                acc.push(null);
+            }
         }
-        return new Array<Block>().concat(acc, space);
+        return acc;
     }, []);
 };
 
-const moveMemoryBlocks = (memoryMap: Block[]): Block[] => {
-    const ret: Block[] = memoryMap;
-    let block: Block | null = null;
-    let nextFreeSpaceIndex: number = -1;
-    for (let i = memoryMap.length - 1; i >= 1; --i) {
-        block = memoryMap[i];
+const defrag = (disk: Disk): Disk => {
+    const defragged = disk.reduce<Disk>((acc) => {
+        // find the first null
+        const firstNullIndex = acc.indexOf(null);
 
-        if (block === FREE_SPACE) {
-            // skip
-            continue;
-        }
-
-        nextFreeSpaceIndex = memoryMap.indexOf(FREE_SPACE);
-        // console.log("block is number", block);
-        // console.log("next free space index", nextFreeSpaceIndex);
-
-        if (nextFreeSpaceIndex === -1 || nextFreeSpaceIndex > i) {
-            break;
-        }
-
-        ret[i] = FREE_SPACE;
-        ret[nextFreeSpaceIndex] = block;
-        // console.log(ret.join(""));
-    }
-
-    return ret;
-};
-/** provide your solution as the return of this function */
-export async function day9a(data: string[]) {
-    const memoryMap = parseData(data);
-    const unpacked = unpackData(memoryMap);
-    const result = moveMemoryBlocks(unpacked);
-    const checksum = result.reduce((acc, block, idx) => {
-        if (block === FREE_SPACE) {
+        // if there are no nulls, we're done
+        if (firstNullIndex === -1) {
             return acc;
         }
-        return (acc += parseInt(block) * idx);
+
+        // if there are nulls, replace the first one with the last non-null
+        if (firstNullIndex !== -1) {
+            let j = acc.length - 1;
+            while (acc[j] === null) {
+                j -= 1;
+            }
+            acc[firstNullIndex] = acc[j]; // swap the first null with the last non-null
+            acc = acc.slice(0, j); // shorten the array to the last non-null
+        }
+
+        return acc;
+    }, disk);
+
+    return new Array(disk.length)
+        .fill(null)
+        .map((_, i) => (i < defragged.length ? defragged[i] : null));
+};
+
+const checksum = (disk: Disk): number => {
+    return disk.reduce((acc, byte, idx) => {
+        if (byte !== null) {
+            acc += byte * idx;
+        }
+        return acc;
     }, 0);
-    return checksum;
+};
+
+/** provide your solution as the return of this function */
+export async function day9a(data: string[]) {
+    const disk = parseData(data);
+    console.log(disk);
+    const defragged = defrag(disk);
+    console.log(defragged);
+    const result = checksum(defrag(parseData(data)));
+    return result;
 }
 
 await runSolution(day9a);
